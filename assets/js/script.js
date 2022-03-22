37e1ee498421049018bfb6172b8ae9fe986a52e3
Mike Williams <williams.michael.256@outlook.com>
2022-03-22 16:16:29 -0500

    api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-  console.log("function was called");
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com',
+		'X-RapidAPI-Key': '92f1976787msh64363e236738f77p158767jsn9e09ea56b99d'
+	}
 };
 
-getUserRepos();
+fetch('https://aerodatabox.p.rapidapi.com/flights/%7BsearchBy%7D/KL1395/2020-06-10', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
