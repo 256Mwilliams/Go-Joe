138a5108bcdf0b65d23992b5e6da3485e63c5bf5
Mike Williams <williams.michael.256@outlook.com>
2022-03-23 19:50:23 -0500

    pulled data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
+//var getUserRepos = function() {
+	//var response = fetch("http://api.aviationstack.com/v1/airlines?access_key=e24c534598f2825f58045c7c74198594&country_name=");
+	//console.log(response);
+//}
+
+
 var getUserRepos = function() {
-	var response = fetch("http://api.aviationstack.com/v1/airlines?access_key=e24c534598f2825f58045c7c74198594&country_name=")
-	console.log(response);
-};
+	var response = fetch("http://api.aviationstack.com/v1/airlines?access_key=e24c534598f2825f58045c7c74198594&country_name").then(function(response) {
+		response.json().then(function(data) {
+			console.log(data);
+		});
+	});
+}
+	
+		
+console.log("outside");
 getUserRepos();
