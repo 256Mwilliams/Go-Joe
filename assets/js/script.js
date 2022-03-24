b1337deb73da6f8d09c478ddd5db526c98504b6d
Mike Williams <williams.michael.256@outlook.com>
2022-03-23 19:11:53 -0500

    fetch data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,5 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com',
-		'X-RapidAPI-Key': '92f1976787msh64363e236738f77p158767jsn9e09ea56b99d'
-	}
+var getUserRepos = function() {
+	var response = fetch("http://api.aviationstack.com/v1/airlines?access_key=e24c534598f2825f58045c7c74198594&country_name=")
+	console.log(response);
 };
-
-fetch('https://aerodatabox.p.rapidapi.com/flights/%7BsearchBy%7D/KL1395/2020-06-10', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+getUserRepos();
