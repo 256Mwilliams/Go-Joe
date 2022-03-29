69326641935df1c63d3784936ba66bd316da2a96
Christian <cnl_1995@icloud.com>
2022-03-29 19:39:01 -0400

    js temp

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ fetch('https://api.sampleapis.com/coffee/hot')
   fetch('https://api.sampleapis.com/coffee/iced')
   .then(response => response.json())
   .then(json => console.log(json))
+
+  function* yourCoffee () {
+    yield 8;
+    yield 9
+    yield 10;
+  }
+
+  var coffeeGenerator = yourCoffee ();
+
+  console.log(coffeeGenerator);
