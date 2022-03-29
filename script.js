0f26f8b99a7fe43541d6bb04653017d58be38ad6
Christian <cnl_1995@icloud.com>
2022-03-29 19:00:34 -0400

    go joe push

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+<<<<<<< Updated upstream
 var APIKey = "AIzaSyBIF_AxPHCOw-e1PotZqgHDRmSsJO11tlE"
 var queryURL = 
 
+=======
+fetch('https://api.sampleapis.com/coffee/hot')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+  fetch('https://api.sampleapis.com/coffee/iced')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+  I
+>>>>>>> Stashed changes
