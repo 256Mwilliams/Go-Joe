f5ff1612027aee189d03f9a2485b75fc8e073222
Christian <cnl_1995@icloud.com>
2022-03-29 19:05:24 -0400

    Omitted previous project code

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Red_Eye
-
-## Aviationstack API
-e24c534598f2825f58045c7c74198594
+To Be Merged Later
--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-var APIKey = "AIzaSyBIF_AxPHCOw-e1PotZqgHDRmSsJO11tlE"
-var queryURL = 
 
-=======
 fetch('https://api.sampleapis.com/coffee/hot')
   .then(response => response.json())
   .then(json => console.log(json))
@@ -10,6 +6,3 @@ fetch('https://api.sampleapis.com/coffee/hot')
   fetch('https://api.sampleapis.com/coffee/iced')
   .then(response => response.json())
   .then(json => console.log(json))
-
-  I
->>>>>>> Stashed changes
