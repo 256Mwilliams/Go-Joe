562483b4efa80198af3b7d250ef5e82521d0e9d8
cnl-95 <98615179+cnl-95@users.noreply.github.com>
2022-03-30 19:15:17 -0400

    merge push

---
+++ script.js
@@ -0,0 +1,67 @@
+   // fetch('https://api.sampleapis.com/coffee/hot')
+   // .then(response => response.json())
+   // .then(json => console.log(json))
+
+   //  fetch('https://api.sampleapis.com/coffee/iced')
+   // .then(response => response.json())
+   // .then(json => console.log(json))
+
+
+
+   //var data;
+  // function getCoffeeResults () {
+  //  fetch('https://api.sampleapis.com/coffee/hot')
+  //  .then(response => response.json())
+ //  }
+
+  let resultOne = document.querySelectorAll('.base')
+
+  resultOne.forEach((checkbox) => { 
+  checkbox.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      console.log(event.target.value)
+    }
+  })
+})
+
+let resultTwo = document.querySelectorAll('.ingredients')
+
+  resultTwo.forEach((checkbox) => { 
+  checkbox.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      console.log(event.target.value)
+    }
+  })
+})
+
+let resultThree = document.querySelectorAll('.roast')
+
+  resultThree.forEach((checkbox) => { 
+  checkbox.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      console.log(event.target.value)
+    }
+  })
+})
+
+let resultFour = document.querySelectorAll('.flavor')
+
+  resultFour.forEach((checkbox) => { 
+  checkbox.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      console.log(event.target.value)
+    }
+  })
+})
+
+let resultFive = document.querySelectorAll('.temp')
+
+  resultFive.forEach((checkbox) => { 
+  checkbox.addEventListener('change', (event) => {
+    if (event.target.checked) {
+      console.log(event.target.value)
+    }
+  })
+})
+
+
